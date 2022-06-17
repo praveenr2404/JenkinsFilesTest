9d2041b223528c8ec2f77cc46956cc48db9faa70
prave <praveenr2404@gmail.com>
2022-06-16 22:52:35 -0400

    app updated

--- JenkinsSub/src/main/java/com/Application.java
+++ JenkinsSub/src/main/java/com/Application.java
@@ -9,12 +9,12 @@ public class Application {
     public static void main(String[] args) {
 
         String basePath = new File(System.getProperty("user.dir")).getAbsolutePath();
-        if(!Files.exists(Paths.get(basePath+"/out/test.txt"))){
+        if(!Files.exists(Paths.get(basePath+"\\out\\test.txt"))){
             basePath = new File(basePath).getParent();
         }
 
         System.out.println("Base Path: "+basePath);
-        String filePath = basePath+"/out/test.txt";
+        String filePath = basePath+"\\out\\test.txt";
         System.out.println("File Path: "+filePath);
         WriteFile.write(filePath);
         System.out.println("Write Success");
