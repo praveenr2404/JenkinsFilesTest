980f69630470ef80aee707c1a617d1109f111651
prave <praveenr2404@gmail.com>
2022-06-16 22:47:38 -0400

    app class updated

--- JenkinsSub/src/main/java/com/Application.java
+++ JenkinsSub/src/main/java/com/Application.java
@@ -1,11 +1,21 @@
 package com;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Application {
     public static void main(String[] args) {
-        String filePath = new File("out/test.txt").getAbsolutePath();
-        System.out.println(filePath);
+
+        String basePath = new File(System.getProperty("user.dir")).getAbsolutePath();
+        if(!Files.exists(Paths.get(basePath+"/out/test.txt"))){
+            basePath = new File(basePath).getParent();
+        }
+
+        System.out.println("Base Path: "+basePath);
+        String filePath = basePath+"/out/test.txt";
+        System.out.println("File Path: "+filePath);
         WriteFile.write(filePath);
         System.out.println("Write Success");
     }
